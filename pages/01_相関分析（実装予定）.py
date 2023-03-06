80ccc6837372462838d98359afa16aa65fcb4fd7
dddd-onigiri <dddd.sailing470@gmail.com>
2023-03-06 21:57:03 +0900

    ファイル名変更

---
+++ pages/01_相関分析（実装予定）.py
@@ -0,0 +1,20 @@
+import math
+
+import streamlit as st
+import pandas as pd
+# 実装予定
+# import matplotlib.pyplot as plt
+from scipy import stats
+from PIL import Image
+
+st.set_page_config(page_title="相関分析", layout="wide")
+
+st.title("相関分析")
+st.caption("Created by Daiki Ito")
+st.write("")
+st.subheader("ブラウザで検定　→　表　→　解釈まで出力できるウェブアプリです。")
+st.write("iPad等でも分析を行うことができます")
+
+st.write("")
+
+st.write("実装予定")
