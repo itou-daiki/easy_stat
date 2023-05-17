08b99eea060087795e3453948e1acc516ba1b14b
itoudaiki <dddd.sailing470@gmail.com>
2023-05-17 21:10:37 +0900

    エラー処理を追加

--- pages/03_t検定（対応なし）.py
+++ pages/03_t検定（対応なし）.py
@@ -292,7 +292,7 @@ with st.form(key='analyze_form'):
             elif df1.iat[n, sign_n] == "†":
                 if df1.iat[n, d0n] > df1.iat[n, d1n]:
                     st.write(
-                        f'{iv}によって【'f'{dn}】には有意な差が生まれる傾向にある'f'（{d0}＞'f'{d1}）')
+                        f'{iv}によって【'f'{dn}】には有意な差が生まれる傾向にある'f'（{d0}＜'f'{d1}）')
                 elif df1.iat[n, d0n] < df1.iat[n, d1n]:
                     st.write(
                         f'{iv}によって【'f'{dn}】には有意な差が生まれる傾向にある'f'（{d1}＜'f'{d0}）')
