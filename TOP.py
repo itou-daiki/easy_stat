eb096cf5cc1f300813ea1199f0b6f9cba249176a
itoudaiki <dddd.sailing470@gmail.com>
2023-08-01 20:47:03 +0900

    エラー処理を追加

--- TOP.py
+++ TOP.py
@@ -2,26 +2,43 @@ import streamlit as st
 
 st.set_page_config(page_title="ブラウザ統計", layout="wide")
 
+# App title and creator
 st.title("ブラウザ統計")
 st.caption("Created by Daiki Ito")
-st.write("")
-st.subheader("ブラウザで統計を行うことができるウェブアプリです。")
-st.write("iPad等で分析を行うことができます")
-st.write("")
 
-st.write("実装済み検定")
-st.write("・相関分析")
-st.write("・ｔ検定（対応なし）")
-st.write("・ｔ検定（対応あり）")
-st.write("")
-st.write("")
-st.write("2023/03/11")
-st.write("　相関分析を実装しました。")
-st.write("")
-st.write("2023/03/06")
-st.write("　ｔ検定（対応あり・なし）を統合しました。")
+# Introduction
+st.markdown("""
+このウェブアプリケーションでは、ブラウザ上で統計分析を行うことができます。iPadなどのデバイスでも対応しています。具体的な機能は以下の通りです：
+""")
 
-st.write("")
-st.write("")
-st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9', 'まで')
-st.write('© 2022-2023 Daiki Ito. All Rights Reserved.')
+# List of implemented tests
+st.markdown("""
+- 簡易データサイエンス
+- 相関分析
+- ｔ検定（対応なし）
+- ｔ検定（対応あり）
+""")
+
+# Updates and history
+st.header("更新履歴")
+
+st.markdown("""
+#### 2023/08/01
+- 簡易データサイエンス機能を実装しました
+- UIとその他の軽微な修正を行いました
+
+#### 2023/03/11
+- 相関分析機能を実装しました
+
+#### 2023/03/06
+- ｔ検定（対応あり・なし）を統合しました
+""")
+
+# Contact and repository info
+st.header("リンク")
+st.markdown("""
+- [フィードバックはこちらまで](https://forms.gle/G5sMYm7dNpz2FQtU9)
+""")
+
+# Copyright
+st.markdown('© 2022-2023 Daiki Ito. All Rights Reserved.')
