f648a622bc3b1e0c36874bf8bc496b73ba818c7e
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-23 12:15:34 +0000

    a

--- pages/00_探索的データ解析（EDA）.py
+++ pages/00_探索的データ解析（EDA）.py
@@ -35,7 +35,7 @@ else:
 if 'df' in locals() or 'df' in globals():
     try:
         # D-taleインスタンスの作成
-        d = dtale.show(df, host='0.0.0.0', port=40000)
+        d = dtale.show(df)
 
         # D-taleアプリをiframe内に埋め込む
         components.iframe(d._main_url, width=1000, height=500)
