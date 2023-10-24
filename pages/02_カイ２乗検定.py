26465c2093ec1620ae450d133e86c0968f738258
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-24 02:21:21 +0000

    大規模な修正

--- pages/02_カイ２乗検定.py
+++ pages/02_カイ２乗検定.py
@@ -1,105 +1,61 @@
-
 import streamlit as st
 import pandas as pd
-from scipy.stats import chi2_contingency
-from PIL import Image
-import japanize_matplotlib
+import scipy.stats as stats
+import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-# フォントのプロパティを設定
-font_prop = mpl.font_manager.FontProperties(fname="ipaexg.ttf")
-# Matplotlibのデフォルトのフォントを変更
-mpl.rcParams['font.family'] = font_prop.get_name()
+import japanize_matplotlib
+from PIL import Image
 
-st.set_page_config(page_title="カイ二乗検定", layout="wide")
+st.set_page_config(page_title="カイ２乗分析", layout="wide")
 
-st.title("カイ二乗検定")
-st.caption("Created by Daiki Ito")
-st.write("")
-st.subheader("ブラウザでカイ二乗検定　→　表　→　解釈まで出力できるウェブアプリです。")
-st.write("iPad等でも分析を行うことができます")
-st.write("")
+st.title("カイ２乗分析ウェブアプリ")
 
-st.subheader("【注意事項】")
-st.write("Excelファイルに不備があるとエラーが出ます")
-st.write('<span style="color:blue">デフォルトでデモ用データの分析ができます。</span>',
-         unsafe_allow_html=True)
-st.write(
-    '<span style="color:blue">ファイルをアップせずに「データフレームの表示」ボタンを押すと　'
-    'デモ用のデータを確認できます。</span>',
-    unsafe_allow_html=True)
-st.write('<span style="color:red">欠損値を含むレコード（行）は自動で削除されます。</span>',
-         unsafe_allow_html=True)
+st.write("度数の偏りを分析することができます")
 
-# デモ用ファイル
-df = pd.read_excel('chi_square_demo.xlsx', sheet_name=0)
+# 分析のイメージ
+image = Image.open('correlation.png')
+st.image(image)
 
-# xlsx, csvファイルのアップロード
-upload_files = st.file_uploader("ファイルアップロード", type=['xlsx', 'csv'])
+# ファイルアップローダー
+uploaded_file = st.file_uploader('ファイルをアップロードしてください (Excel or CSV)', type=['xlsx', 'csv'])
 
-# xlsx, csvファイルの読み込み → データフレームにセット
-if upload_files:
-    # dfを初期化
-    df.drop(range(len(df)))
-    # ファイルの拡張子によって読み込み方法を変える
-    if upload_files.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        # xlsxファイルの読み込み → データフレームにセット
-        df = pd.read_excel(upload_files, sheet_name=0)
-    elif upload_files.type == 'text/csv':
-        # csvファイルの読み込み → データフレームにセット
-        df = pd.read_csv(upload_files)
-    # 欠損値を含むレコードを削除
-    df.dropna(how='any', inplace=True)
+# デモデータを使うかどうかのチェックボックス
+use_demo_data = st.checkbox('デモデータを使用')
 
-# データフレーム表示ボタン
-if st.checkbox('データフレームの表示（クリックで開きます）'):
-    st.dataframe(df, width=0)
+# データフレームの作成
+df = None
+if use_demo_data:
+    df = pd.read_excel('chi_square_demo.xlsx', sheet_name=0)
+    st.write(df.head())
+else:
+    if uploaded_file is not None:
+        if uploaded_file.type == 'text/csv':
+            df = pd.read_csv(uploaded_file)
+            st.write(df.head())
+        else:
+            df = pd.read_excel(uploaded_file)
+            st.write(df.head())
 
-# 変数選択フォーム
-with st.form(key='variable_form'):
-    st.subheader("分析に使用する変数の選択")
-    # 複数選択
-    Variables = st.multiselect('変数（複数選択可）', df.columns.tolist())
-    vRange = len(Variables)
+if df is not None:
+    # カテゴリ変数の抽出
+    categorical_cols = df.select_dtypes(include=['object', 'category']).columns.tolist()
 
-    # 確認ボタンの表示
-    CHECK_btn = st.form_submit_button('確認')
+    # カテゴリ変数の選択
+    st.subheader("カテゴリ変数の選択")
+    selected_cols = st.multiselect('カテゴリ変数を選択してください', categorical_cols)
 
-# 分析前の確認フォーム
-with st.form(key='check_form'):
-    if CHECK_btn:
-        st.subheader('【分析前の確認】')
+    if len(selected_cols) > 0:
+        for col in selected_cols:
+            st.subheader(f"{col} の度数分布")
+            # 度数分布のバープロット
+            plt.figure(figsize=(8, 6))
+            sns.countplot(data=df, x=col)
+            st.pyplot(plt)
 
-        n = 0
-        for ViewCheck in range(vRange):
-            st.write(
-                f'● 【'f'{(Variables[n])}】')
-            n += 1
-        st.write('    '+'これらの変数の出現度数に有意な差が生まれるか検定します。')
-
-    # 分析実行ボタンの表示
-    ANALYZE_btn = st.form_submit_button('分析実行')
-
-# 分析結果表示フォーム
-with st.form(key='analyze_form'):
-    if ANALYZE_btn:
-        st.subheader('【分析結果】')
-
-        # chi-square test
-        chi2, p, dof, expected = chi2_contingency(df[Variables])
-
-        # Create dataframe
-        data = {'カイ２乗値': [chi2], 'p値': [p], '自由度': [dof]}
-        result_df = pd.DataFrame(data)
-
-        # Expected counts
-        expected_df = pd.DataFrame(expected, columns=Variables)
-
-        st.dataframe(result_df)
-        st.dataframe(expected_df)
-
-    ANALYZE_btn = st.form_submit_button('OK')
-
-st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9',
-         'まで')
-st.write('© 2022-2023 Daiki Ito. All Rights Reserved.')
+            # カイ２乗分析
+            crosstab = pd.crosstab(df[col], columns="count")
+            chi2, p_value, dof, expected = stats.chi2_contingency(crosstab)
+            st.write(f"カイ２乗統計量: {chi2:.2f}")
+            st.write(f"P値: {p_value:.2f}")
+    else:
+        st.warning('少なくとも1つのカテゴリ変数を選択してください。')
