24e4d47d1c2216a81a3b607fe5bd17dff1f25eaf
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-24 21:28:27 +0000

    修正

--- pages/03_t検定（対応なし）.py
+++ pages/03_t検定（対応なし）.py
@@ -114,6 +114,81 @@ if df is not None:
             # 要約統計量（サマリ）のデータフレームを表示
             st.write(df0.style.format("{:.2f}"))
 
+            st.write('【平均値の差の検定】')
+            groups = df[cat_var].iloc[:, 0].unique().tolist()
+
+            # 結果を保存するデータフレームの初期化
+            columns = ['全体M', '全体S.D', f'{groups[0]}M', f'{groups[0]}S.D',
+                    f'{groups[1]}M', f'{groups[1]}S.D', 'df', 't', 'p', 'sign', 'd']
+            df_results = pd.DataFrame(columns=columns)
+
+            for var in num_vars:
+                series = df[var]
+                group0_data = df[df[cat_var[0]] == groups[0]][var]
+                group1_data = df[df[cat_var[0]] == groups[1]][var]
+
+                ttest_result = stats.ttest_ind(group0_data, group1_data, equal_var=False)
+                overall_mean = series.mean()
+                overall_std = series.std()
+                g0_mean = group0_data.mean()
+                g0_std = group0_data.std()
+                g1_mean = group1_data.mean()
+                g1_std = group1_data.std()
+                effect_size = abs(g0_mean - g1_mean) / overall_std
+
+                if ttest_result.pvalue < 0.01:
+                    significance = '**'
+                elif ttest_result.pvalue < 0.05:
+                    significance = '*'
+                elif ttest_result.pvalue < 0.1:
+                    significance = '†'
+                else:
+                    significance = 'n.s.'
+
+                results_row = {
+                    '全体M': overall_mean,
+                    '全体S.D': overall_std,
+                    f'{groups[0]}M': g0_mean,
+                    f'{groups[0]}S.D': g0_std,
+                    f'{groups[1]}M': g1_mean,
+                    f'{groups[1]}S.D': g1_std,
+                    'df': len(series) - 1,
+                    't': abs(ttest_result.statistic),
+                    'p': ttest_result.pvalue,
+                    'sign': significance,
+                    'd': effect_size
+                }
+
+                df_results.loc[var] = results_row
+
+            # 結果の表示
+            st.write(df_results.style.format("{:.2f}"))
+
+            # サンプルサイズの表示
+            st.write('サンプルサイズ')
+            st.write(f'全体N ＝ {len(df)}')
+            st.write(f'● {groups[0]}： {len(group0_data)}')
+            st.write(f'● {groups[1]}： {len(group1_data)}')
+
+            # グラフの描画
+            sns.set(style="whitegrid")
+            for var in num_vars:
+                data = pd.DataFrame({
+                    '群': groups,
+                    '平均値': [df_results.at[var, f'{groups[0]}M'], df_results.at[var, f'{groups[1]}M']],
+                    '誤差': [df_results.at[var, f'{groups[0]}S.D'], df_results.at[var, f'{groups[1]}S.D']]
+                })
+
+                fig, ax = plt.subplots(figsize=(8, 6))
+                sns.barplot(x='群', y='平均値', data=data, ax=ax, capsize=0.1, errcolor='black', errwidth=1)
+                ax.errorbar(x=data['群'], y=data['平均値'], yerr=data['誤差'], fmt='none', c='black', capsize=3)
+                ax.set_title(f'平均値の比較： {var}')
+                st.pyplot(fig)
+
+
+
+
+
 
 
 st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9', 'まで')
