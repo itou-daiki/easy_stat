f5ea1c6b7167fdc0ee3994e618b1ad1cc29eacf3
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-24 12:47:01 +0000

    修正

--- pages/02_カイ２乗検定.py
+++ pages/02_カイ２乗検定.py
@@ -126,6 +126,10 @@ if df is not None:
         title=f'【{selected_col1}】 と 【{selected_col2}】 の観測度数ヒートマップ'
     )
 
+    # ヒートマップに観測度数を表示
+    fig_heatmap.update_traces(text=crosstab.iloc[:-1, :-1].values, selector=dict(type='heatmap'))
+    fig_heatmap.update_layout(scene=dict(aspectmode="manual", aspectratio=dict(x=1, y=1, z=0.05)))
+
     # ヒートマップの表示
     st.plotly_chart(fig_heatmap)
 
