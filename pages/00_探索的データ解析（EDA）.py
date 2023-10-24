f1b17a0f6c5d0c60baebe6cda12c0bb494b5eaf6
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-24 01:40:51 +0000

    表記を修正

--- pages/00_探索的データ解析（EDA）.py
+++ pages/00_探索的データ解析（EDA）.py
@@ -146,7 +146,7 @@ if df is not None:
         fig.update_layout(
             xaxis_title=cat_var1,
             yaxis_title=f'平均 {num_var}',
-            margin=dict(l=0, r=0, t=60, b=0)
+            margin=dict(l=0, r=0, t=60, b=0),
             facet_col_spacing=0.05  # 列間のスペースを増やす
         )
 
