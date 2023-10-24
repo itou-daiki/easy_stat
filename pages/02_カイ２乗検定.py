b38396085402312ccc5939cfce9ad3e93388d292
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-24 02:26:31 +0000

    修正

--- pages/02_カイ２乗検定.py
+++ pages/02_カイ２乗検定.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import scipy.stats as stats
-import seaborn as sns
+import plotly.express as px
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 from PIL import Image
@@ -47,10 +47,9 @@ if df is not None:
     if len(selected_cols) > 0:
         for col in selected_cols:
             st.subheader(f"{col} の度数分布")
-            # 度数分布のバープロット
-            plt.figure(figsize=(8, 6))
-            sns.countplot(data=df, x=col)
-            st.pyplot(plt)
+            # 度数分布のバープロット (Plotlyを使用)
+            fig = px.bar(df, x=df[col].value_counts().index, y=df[col].value_counts().values, labels={df[col].name: '度数'})
+            st.plotly_chart(fig)
 
             # カイ２乗分析
             crosstab = pd.crosstab(df[col], columns="count")
@@ -61,8 +60,20 @@ if df is not None:
                 "項目": ["カイ二乗統計量", "P値", "自由度"],
                 "値": [chi2, p_value, dof]
             })
+
+            # クロス表を表示
+            st.write('クロス表:')
+            st.write(crosstab)
             
             # 結果のデータフレームを表示
+            st.write('カイ二乗分析結果:')
             st.dataframe(results_df)
+            
+            # 有意差がある場合は、明示的に表示
+            if p_value < 0.05:
+                st.markdown(f'### {col} には **有意差** があります!')
     else:
         st.warning('少なくとも1つのカテゴリ変数を選択してください。')
+
+st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9', 'まで')
+st.write('© 2022-2023 Daiki Ito. All Rights Reserved.')
