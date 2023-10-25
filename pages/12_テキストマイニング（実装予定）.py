c77502918b4fc8bfe2b8a6bcbdf31a6c10fdf448
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-25 00:25:06 +0000

    修正

--- pages/12_テキストマイニング（実装予定）.py
+++ pages/12_テキストマイニング（実装予定）.py
@@ -56,7 +56,16 @@ if df is not None:
     npt = nlplot.NLPlot(df, target_col=selected_text)
         
     # ワードクラウドの作成と表示
-    wordcloud = npt.wordcloud(width=800, height=400)
+
+    # MeCabの初期化
+    mecab = MeCab.Tagger("-Owakati")
+
+    # テキストデータの抽出と単語の分割
+    text_data = df[selected_text].str.cat(sep=' ')
+    words = mecab.parse(text_data).strip()
+
+    # ワードクラウドの作成と表示
+    wordcloud = WordCloud(width=800, height=400, background_color='white', collocations=False, font_path='/usr/share/fonts/truetype/fonts-japanese-gothic.ttf').generate(words)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation="bilinear")
     ax.axis('off')
@@ -74,13 +83,17 @@ if df is not None:
     st.subheader('カテゴリ別の分析')
     grouped = df.groupby(selected_category)
     for name, group in grouped:
-        st.write(f'Category: {name}')
+        st.write(f'＜カテゴリ： {name}＞')
             
         # ワードクラウドと共起ネットワークの作成と表示 (カテゴリ別)
         npt_group = nlplot.NLPlot(group, target_col=selected_text)
             
-        # ワードクラウドの作成と表示
-        wordcloud_group = npt_group.wordcloud(width=800, height=400)
+        # テキストデータの抽出と単語の分割 (カテゴリ別)
+        text_data_group = group[selected_text].str.cat(sep=' ')
+        words_group = mecab.parse(text_data_group).strip()
+
+        # ワードクラウドの作成と表示 (カテゴリ別)
+        wordcloud_group = WordCloud(width=800, height=400, background_color='white', collocations=False, font_path='/usr/share/fonts/truetype/fonts-japanese-gothic.ttf').generate(words_group)
         fig, ax = plt.subplots()
         ax.imshow(wordcloud_group, interpolation="bilinear")
         ax.axis('off')
