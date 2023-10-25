e31d30372f63df0b8ee9c6638518bcd7fdeafe85
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-25 10:51:58 +0000

    a

--- pages/12_テキストマイニング.py
+++ pages/12_テキストマイニング.py
@@ -124,7 +124,6 @@ if df is not None:
     wordcloud1 = npt.wordcloud(
         width=800, height=400, 
         max_words=max_words,
-        collocations=False, 
         font_path=font_path,
         min_font_size=4,
         stopwords=stopwords_list
