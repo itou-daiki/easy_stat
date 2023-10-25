fb455337ffba46c86f8f202949cb6c5486faf98e
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-25 04:10:11 +0000

    修正

--- pages/12_テキストマイニング（実装予定）.py
+++ pages/12_テキストマイニング（実装予定）.py
@@ -63,7 +63,7 @@ if df is not None:
         
     # ストップワードの定義 (KH Coderのデフォルトの日本語ストップワードを参考に簡易的に定義)
     STOPWORDS = set(["する", "なる", "ある", "こと", "これ", "それ", "もの", "ため", "ところ", "やる", "れる", "られる","の","を","し","に","です","は"])
-    STOPWORDS_net = list(STOPWORDS)
+    stopwords_list = list(STOPWORDS) + npt.default_stopwords
 
     # MeCabの初期化
     mecab = MeCab.Tagger("-Owakati")
@@ -112,8 +112,7 @@ if df is not None:
         
     # 共起ネットワークの作成と表示
     try:
-        STOPWORDS = set(["する", "なる", "ある", "こと", "これ", "それ", "もの", "ため", "ところ", "やる", "れる", "られる","の","を","し","に","です","は"])
-        network = npt.build_graph(stopwords=STOPWORDS,min_edge_frequency=10)
+        network = npt.build_graph(stopwords=stopwords_list,min_edge_frequency=10)
         fig = npt.co_network(network, sizing=100,node_size='adjacency_frequency', color_palette='hls')
         st.pyplot(fig)
     except ValueError as e:
@@ -170,8 +169,7 @@ if df is not None:
             
         # 共起ネットワークの作成と表示
         try:
-            STOPWORDS = set(["する", "なる", "ある", "こと", "これ", "それ", "もの", "ため", "ところ", "やる", "れる", "られる","の","を","し","に","です","は"])
-            network_group = npt_group.build_graph(stopwords=STOPWORDS,min_edge_frequency=10)
+            network_group = npt_group.build_graph(stopwords=stopwords_list,min_edge_frequency=10)
             fig = npt_group.co_network(network_group, sizing=100,node_size='adjacency_frequency', color_palette='hls')
             st.pyplot(fig)
         except ValueError as e:
