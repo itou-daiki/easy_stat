e2ffde0c0dd74e7ebf8a7638c5de7217e978effa
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-25 12:24:19 +0000

    a

--- pages/03_t検定（対応なし）.py
+++ pages/03_t検定（対応なし）.py
@@ -187,11 +187,11 @@ if df is not None:
 
             def add_bracket(ax, x1, x2, y, text):
                 bracket_length = 2
-                ax.annotate("", xy=(x1, y), xycoords='data',
-                            xytext=(x1, y + bracket_length), textcoords='data',
+                ax.annotate("", xy=(x1 + 2, y), xycoords='data',
+                            xytext=(x1 + 2, y + bracket_length), textcoords='data',
                             arrowprops=dict(arrowstyle="-", linewidth=1))
-                ax.annotate("", xy=(x2, y), xycoords='data',
-                            xytext=(x2, y + bracket_length), textcoords='data',
+                ax.annotate("", xy=(x2 - 2, y), xycoords='data',
+                            xytext=(x2 - 2, y + bracket_length), textcoords='data',
                             arrowprops=dict(arrowstyle="-", linewidth=1))
                 ax.annotate("", xy=(x1, y + bracket_length), xycoords='data',
                             xytext=(x2, y + bracket_length), textcoords='data',
