7e1bf107b1a6ddc7445d2ad01fb9515351af8bf8
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-26 13:23:53 +0900

    ダウンロード機能を修正

--- pages/00_データクレンジング.py
+++ pages/00_データクレンジング.py
@@ -26,6 +26,8 @@ if uploaded_file is not None:
     st.subheader('元のデータ')
     st.write(data)
 
+    processed_data = data
+
     remove_outliers_option = st.checkbox('外れ値の削除')
     data_cleansing_option = st.checkbox('欠損値の削除')
     remove_empty_columns_option = st.checkbox('値が入っていないカラム（列）の削除')
@@ -34,29 +36,29 @@ if uploaded_file is not None:
         process_history = {}  # 処理の履歴を格納する辞書
 
         if remove_outliers_option:
-            num_cols = data.select_dtypes(include=np.number).columns
+            num_cols = processed_data.select_dtypes(include=np.number).columns
             if not num_cols.empty:
-                Q1 = data[num_cols].quantile(0.25)
-                Q3 = data[num_cols].quantile(0.75)
+                Q1 = processed_data[num_cols].quantile(0.25)
+                Q3 = processed_data[num_cols].quantile(0.75)
                 IQR = Q3 - Q1
-                outlier_condition = ((data[num_cols] < (Q1 - 1.5 * IQR)) | (data[num_cols] > (Q3 + 1.5 * IQR)))
-                data = data[~outlier_condition.any(axis=1)]
+                outlier_condition = ((processed_data[num_cols] < (Q1 - 1.5 * IQR)) | (processed_data[num_cols] > (Q3 + 1.5 * IQR)))
+                processed_data = processed_data[~outlier_condition.any(axis=1)]
                 process_history['【外れ値の削除】'] = '＜外れ値を削除したカラム（列）＞:\n{}'.format(",\n　".join(num_cols))
             else:
                 st.warning('外れ値を削除する数値列がありません')
 
         if data_cleansing_option:
-            data = data.dropna()
-            data = data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+            processed_data = processed_data.dropna()
+            processed_data = processed_data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
             process_history['【欠損値の削除】'] = '欠損値の削除と文字列の空白の削除を行いました'
 
         if remove_empty_columns_option:
-            empty_columns = data.columns[data.isna().all()].tolist()
-            data = data.dropna(axis=1, how='all')
+            empty_columns = processed_data.columns[data.isna().all()].tolist()
+            processed_data = processed_data.dropna(axis=1, how='all')
             process_history['【値が入っていないカラム（列）の削除】'] = '＜削除されたカラム＞:\n{}'.format(",\n　".join(empty_columns))
 
         st.subheader('処理済みのデータ')
-        st.write(data)
+        st.write(processed_data)
 
         st.subheader('処理の履歴')
         for process, details in process_history.items():
@@ -64,7 +66,7 @@ if uploaded_file is not None:
 
         file_format = st.selectbox('ダウンロードするファイル形式を選択', ['Excel', 'CSV'])
         if file_format == 'CSV':
-            csv_data = data.to_csv(index=False)
+            csv_data = processed_data.to_csv(index=False)
             st.download_button(
                 label="処理済みデータをダウンロード",
                 data=csv_data,
@@ -73,7 +75,7 @@ if uploaded_file is not None:
             )
         else:
             excel_io = io.BytesIO()  # BytesIOオブジェクトを作成
-            data.to_excel(excel_io, index=False)  # ExcelデータをBytesIOオブジェクトに書き込む
+            processed_data.to_excel(excel_io, index=False)  # ExcelデータをBytesIOオブジェクトに書き込む
             excel_data = excel_io.getvalue()  # BytesIOオブジェクトからバイトデータを取得
             st.download_button(
                 label="処理済みデータをダウンロード",
