39fb0c7d9962e91eec9ace37672ff26b3bbba8ba
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-26 11:50:17 +0000

    修正

--- pages/04_t検定（対応なし）.py
+++ pages/04_t検定（対応なし）.py
@@ -251,7 +251,7 @@ if df is not None:
 
             # 結合された図の縦軸を揃える
             
-            y_max = max([max(data['平均値']) + max(data['誤差']) *1.1 for var in num_vars])
+            y_max = max([max(data['平均値']) + max(data['誤差']) *1.5 for var in num_vars])
             fig, axs = plt.subplots(1, len(num_vars), figsize=(8*len(num_vars), 6), sharey=True)  # sharey=Trueで縦軸を揃える
             for i, var in enumerate(num_vars):
                 ax = axs[i]  # 各図の座標軸を取得
@@ -280,7 +280,7 @@ if df is not None:
                     significance_text = "n.s."
 
                 add_bracket(ax, 0, 1, max(data['平均値']) + max(data['誤差']) + 5, significance_text)
-                ax.set_ylim([0, y_max*1.1])  # 各図の縦軸の最大値を揃える
+                ax.set_ylim([0, y_max*1.5])  # 各図の縦軸の最大値を揃える
                 ax.spines['top'].set_visible(False)  # 上の枠線を消す
                 ax.spines['right'].set_visible(False)  # 右の枠線を消す
                 ax.spines['left'].set_visible(False)  # 左の枠線を消す
