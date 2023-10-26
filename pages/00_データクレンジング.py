838a6d88bcbb85e2bb7c406fb8ef689b313e1653
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-10-26 13:36:41 +0900

    表記を修正

--- pages/00_データクレンジング.py
+++ pages/00_データクレンジング.py
@@ -61,11 +61,22 @@ if uploaded_file is not None:
         st.write(processed_data)
 
         with st.expander("処理の履歴"):  # アコーディオンウィジェットを使用
-                # テーブル形式での表示
-                process_history_df = pd.DataFrame(list(process_history.items()), columns=["Process", "Details"])
-                st.table(process_history_df)  # 処理の履歴をテーブル形式で表示
-                for process, details in process_history.items():
-                    st.markdown(f"**{process}**:\n{details}\n\n")
+            # 外れ値の削除のテーブル
+            if '外れ値の削除' in process_history:
+                st.write('＜外れ値の削除＞')
+                outlier_cols = process_history['&#8203;``【oaicite:4】``&#8203;'].split('\n')[1].split('、')
+                st.table(pd.DataFrame(outlier_cols, columns=['外れ値を削除したカラム（列）']))
+            
+            # 欠損値の削除のテーブル
+            if '欠損値の削除' in process_history:
+                st.write('＜欠損値の削除＞')
+                st.table(pd.DataFrame(columns=['詳細'], data=[process_history['&#8203;``【oaicite:2】``&#8203;']]))
+            
+            # 値が入っていないカラム（列）の削除のテーブル
+            if '値が入っていないカラム（列）の削除' in process_history:
+                st.write('＜値が入っていないカラム（列）の削除＞')
+                empty_cols = process_history['&#8203;``【oaicite:0】``&#8203;'].split('\n')[1].split('、')
+                st.table(pd.DataFrame(empty_cols, columns=['削除されたカラム']))
 
         file_format = st.selectbox('ダウンロードするファイル形式を選択', ['Excel', 'CSV'])
         if file_format == 'CSV':
