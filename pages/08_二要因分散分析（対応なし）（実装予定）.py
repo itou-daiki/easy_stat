d601f9103ee9fd897aab952e7f48256af3f9eedc
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-11-22 05:25:41 +0000

    修正

--- pages/08_二要因分散分析（対応なし）（実装予定）.py
+++ pages/08_二要因分散分析（対応なし）（実装予定）.py
@@ -1,16 +1,15 @@
-# 必要なライブラリのインポート
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import japanize_matplotlib
-from PIL import Image
 from scipy import stats
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from statistics import median, variance
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+from PIL import Image
 
 # Streamlitのページ設定
 st.set_page_config(page_title="二要因分散分析(対応なし)", layout="wide")
@@ -53,7 +52,7 @@ if df is not None:
 
     # カテゴリ変数の選択
     st.subheader("カテゴリ変数の選択")
-    cat_vars = st.multiselect('2つのカテゴリ変数を選択してください', categorical_cols, format_func=lambda x: x)
+    cat_vars = st.multiselect('2つのカテゴリ変数を選択してください', categorical_cols, format_func=lambda x: x, default=categorical_cols[:2])
 
     # 数値変数の選択
     st.subheader("数値変数の選択")
@@ -71,12 +70,15 @@ if df is not None:
         if st.button('分散分析の実行'):
             st.subheader('【分析結果】')
 
-            # two-way ANOVAの実行
-            formula = f'{num_var} ~ C({cat_vars[0]}) + C({cat_vars[1]}) + C({cat_vars[0]}):C({cat_vars[1]})'
-            model = ols(formula, df).fit()
-            anova_results = anova_lm(model, typ=2)
-
-            st.write(anova_results)
+            # formulaの生成
+            # カテゴリ変数が日本語や特殊文字を含む場合、バッククォートで囲む
+            formula = f'Q("{num_var}") ~ C(Q("{cat_vars[0]}")) + C(Q("{cat_vars[1]}")) + C(Q("{cat_vars[0]}")):C(Q("{cat_vars[1]}"))'
+            try:
+                model = ols(formula, df).fit()
+                anova_results = anova_lm(model, typ=2)
+                st.write(anova_results)
+            except Exception as e:
+                st.error(f"分析中にエラーが発生しました: {e}")
 
             # TukeyのHSDテストと可視化
             st.write("【多重比較の結果】")
