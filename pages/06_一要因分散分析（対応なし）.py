cca71867e6964b63d598ec3bac6ec3d563b9a648
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2023-12-02 04:23:17 +0000

    修正

--- pages/06_一要因分散分析（対応なし）.py
+++ pages/06_一要因分散分析（対応なし）.py
@@ -253,7 +253,7 @@ if df is not None:
                 errors = groups[num_var].std()
 
                 # y軸の上限値を計算
-                # y_max = max(means.values + np.array(errors.values))
+                ybar_max = max(means.values + np.array(errors.values))
                 y_max = max(means + errors * 3)
 
                 # ブラケットとアノテーションに必要な追加スペースを計算
@@ -273,7 +273,7 @@ if df is not None:
                 bars = ax.bar(x=means.index, height=means.values, yerr=errors.values, capsize=5)
 
                 # y軸の最大値に基づくブラケットの開始位置を設定
-                y_bracket_start = y_max + 0.5
+                y_bracket_start = ybar_max + 0.5
 
                 # ブラケットの位置を格納するリストを初期化
                 bracket_positions = []
