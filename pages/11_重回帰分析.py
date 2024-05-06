0042dae459f7be4e46d7fd38ddbc8ea72aef8acb
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-05-06 01:51:24 +0000

    表記を修正

--- pages/11_重回帰分析.py
+++ pages/11_重回帰分析.py
@@ -148,3 +148,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+st.write('ご意見・ご要望は→', 'https://forms.gle/G5sMYm7dNpz2FQtU9', 'まで')
+# Copyright
+st.subheader('© 2022-2024 Dit-Lab.(Daiki Ito). All Rights Reserved.')
+st.write("easyStat: Open Source for Ubiquitous Statistics")
+st.write("Democratizing data, everywhere.")
+st.write("")
+st.subheader("In collaboration with our esteemed contributors:")
+st.write("・Toshiyuki")
+st.write("With heartfelt appreciation for their dedication and support.")
