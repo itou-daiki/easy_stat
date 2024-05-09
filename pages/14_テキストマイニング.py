d5ecafbeefc183ca2379857c4952fed22e219b8a
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-05-09 12:24:22 +0000

    共起ネットワークう

--- pages/14_テキストマイニング.py
+++ pages/14_テキストマイニング.py
@@ -159,12 +159,13 @@ if df is not None:
         ax.axis('off')
         st.pyplot(fig)
 
-        # 共起ネットワークの作成と表示
+        # 共起ネットワークの作成と表示 (カテゴリ別)
         st.subheader('【共起ネットワーク】')
-        min_edge_frequency_all = st.slider('最小エッジ頻度', 1, 100, 1, key='co_network_all')
-        G = npt.build_graph(df, min_edge_frequency=min_edge_frequency_all)
-        fig_co_network_all = npt.co_network(
-            G,
+        min_edge_frequency_group = st.slider('最小エッジ頻度',1, 100, 100, key=f'co_network_group_{name}')
+        npt_group = nlplot.NLPlot(group, target_col=selected_text)
+        G_group = npt_group.build_graph(group, min_edge_frequency=min_edge_frequency_group)
+        fig_co_network_group = npt_group.co_network(
+            G_group,
             sizing=100,
             node_size='adjacency_frequency',
             color_palette='hls',
@@ -172,7 +173,7 @@ if df is not None:
             height=700,
             save=False
         )
-        st.write(fig_co_network_all)
+        st.write(fig_co_network_group)
 
         # 名詞の度数を棒グラフで表示 (カテゴリ別)
         st.subheader('【名詞の出現度数】')
